91610d90a46c0c8790ef19922197ca844eebdb7a
BetoDantas <109151236+BetoDantas@users.noreply.github.com>
2023-09-18 15:45:36 -0300

    Update ex_wagner2.py

---
+++ ex_wagner2.py
@@ -0,0 +1,23 @@
+import customtkinter
+customtkinter.set_appearance_mode('dark')
+customtkinter.set_default_color_theme("drak-blue")
+
+window = customtkinter.CTk()
+window.geometry('500x300')
+
+def clique():
+    print("Login efuteuado com sucesso")
+
+text = custmtkinter.CTkLabel(window,text='Usuário')
+text.pack(padx=10, pady=10)
+
+email = customtkinter.CTkEntry(window, placeholdertext = "seu usuário")
+email.pack(padx=10,pady=10)
+
+senha = customtkinter.CTkEntry(window, placeholdertext = "Senha", show = '*')
+senha.pack(padx=10,pady=10)
+
+button = customtkinter.CTkButton(window, text='Entrar', command=clique())
+button.pack(padx=10, pady=10)
+
+window.mainloop()
